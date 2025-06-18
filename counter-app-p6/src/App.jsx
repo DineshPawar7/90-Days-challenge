74d00e23aafe9acc121d161c290b6f207857762e
Dinesh Pawar <dsp70458@gmail.com>
2025-06-18 21:10:33 +0530

    Kuch meaningful message

--- counter-app-p6/src/App.jsx
+++ counter-app-p6/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div className='text-center p-[2rem]'>
       <h1>Simple Counter</h1>
+      <h2>simple test to merge</h2>
       <p>Count: {count}</p>
       <button className='m-1 px-2' onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
