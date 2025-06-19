26084c647c5a5585164f2b8a47fde9b371a06ec2
Dinesh Pawar <dsp70458@gmail.com>
2025-06-19 07:59:46 +0530

    test commit to test

--- counter-app-p6/src/App.jsx
+++ counter-app-p6/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div className='text-center p-[2rem]'>
       <h1>Simple Counter</h1>
+      <p>simple commit to test</p>
       <p>Count: {count}</p>
       <button className='m-1 px-2' onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(count + 1)}>+</button>
