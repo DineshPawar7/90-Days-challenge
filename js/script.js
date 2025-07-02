ea29aaf27cca01e2d8cdd1d687d49adb811c8ff4
Dinesh Pawar <dsp70458@gmail.com>
2025-07-02 18:21:40 +0530

    refactor: remove function implementation and comments for clarity

--- js/script.js
+++ js/script.js
@@ -1,11 +1,109 @@
-function dinu(str) {
-    return str.split('').reverse().join('');
 
-}
 
-const input = 'hello';
-const reversed = dinu(input);
-console.log(reversed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Object Copy Behavior (Reference vs Value)
+// let a = 5;
+// let b = a;
+// b = 10;
+
+// console.log(a);
+// console.log(b)
+
+
+
+
+
+
+
+
+// // Reference Types – Object & Array
+// let person = {
+//     name: "Dinesh",
+//     age:"22"
+// };
+
+// let numbers = [1,2,3,4]
+
+// console.log(typeof person)
+// console.log(typeof numbers)
+
+// console.log(Array.isArray(numbers))
+
+
+
+
+
+// let num = 100;
+// let str = "hello";
+// let bool = true;
+// let und;
+// let nul = null;
+// let big = 3465235473432463332535354n;
+// let sym = Symbol("hey")
+// {
+//     [Symbol] = "dinesh"
+// }
+
+// console.log(typeof num);   // number
+// console.log(typeof str);   // string
+// console.log(typeof bool);  // boolean
+// console.log(typeof und);   // undefined
+// console.log(typeof nul);   // object ❗ (JS quirk)
+// console.log(typeof big);   // bigint
+// console.log(typeof sym);   // symbol
+
+
+
+// // Re-declaration vs Re-assignment
+// var x = 5;
+// var x = 10;
+
+// let y = 15;
+
+// const z = 25;
+
+
+
+
+
+// //Scope Difference
+// function tetVarLetConst() {
+//     if (true) {
+//         var a = 10;
+//         let b = 20;
+//         const c = 30;
+//     }
+//     console.log(a);
+//     console.log(b)
+//     console.log(c)
+// }
+
+
+
+
+
+
+
+// function dinu(str) {
+//     return str.split('').reverse().join('');
+
+// }
+
+// const input = 'hello';
+// const reversed = dinu(input);
+// console.log(reversed)
 
 
 
