f6e4fe9c804dadc341ee7712f196b36716b935a9
Dinesh Pawar <dsp70458@gmail.com>
2025-07-02 16:30:34 +0530

    refactor: restructure script.js to improve function definition and remove commented code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-// reasignation
+function dinu(str) {
+    return str.split('').reverse().join('');
 
-let a = 34;
-a = 4554;
+}
 
-// let a = 54  //ERROR
+const input = 'hello';
+const reversed = dinu(input);
+console.log(reversed)
 
 
-// REVALUATION
 
-var b = 34;
-var v = 434;
+
+
+
+
+// // reasignation
+
+// let a = 34;
+// a = 4554;
+
+// // let a = 54  //ERROR
+
+
+// // REVALUATION
+
+// var b = 34;
+// var v = 434;
+
+
+
